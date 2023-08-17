7112bc6623df151f888418074bec28bd22c6171c
TatjanaPro <tatjaana.pronina@gmail.com>
2023-08-17 13:24:13 +0300

    changes after code review

--- auction-app/src/main/java/com/auctionapp/product/repository/ProductRepo.java
+++ auction-app/src/main/java/com/auctionapp/product/repository/ProductRepo.java
@@ -3,6 +3,7 @@ package com.auctionapp.product.repository;
 import com.auctionapp.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
