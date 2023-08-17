874f92b06501075a23e3ff64e88102f2cf2dca87
Karolina Belousova <karolina.belousova@gmail.com>
2023-08-17 20:20:48 +0300

    added Auction entity

---
+++ auction-app/src/main/java/com/auctionapp/model/auction/Auction.java
@@ -0,0 +1,36 @@
+package com.auctionapp.model.auction;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+public class Auction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Basic
+    @Column(name = "type")
+    private String type;
+
+    @Basic
+    @Column (name = "start_time")
+    private Date startTime;
+
+    @Basic
+    @Column (name = "end_time")
+    private Date endTime;
+
+    @JoinColumn(name = "user_id")
+    private Long userId;
+
+}
