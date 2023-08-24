9250b96d4f3c8a2d1debd41237f7fa0ec9102ece
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2023-08-24 16:43:12 +0300

    removed unused method for testing purpose

--- auction-app/src/main/java/com/auctionapp/repository/ProductRepository.java
+++ auction-app/src/main/java/com/auctionapp/repository/ProductRepository.java
@@ -10,7 +10,4 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE lower(p.name) LIKE lower(concat('%', :name, '%'))")
-    List<Product> findByAllNames(String name);
-
 }
