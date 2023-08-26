bf199cb343b166ea3a40cd1ed736c8c00b021ac2
LisaCourtade <courtadelisa98@gmail.com>
2023-08-26 16:08:25 +0300

    design fixes

--- auction-ui/src/routes/home/AuctionCard.component.jsx
+++ auction-ui/src/routes/home/AuctionCard.component.jsx
@@ -8,33 +8,34 @@ const AuctionCardComponent = ({ auction }) => {
     const {name, description, category, userId, url} = product;
     const {id: auctionUserId, username, email, password, roles} = user;
 
-    const formattedStartTime = format(new Date(startTime), 'MMM dd, yyyy hh:mm a');
-    const formattedEndTime = format(new Date(endTime), 'MMM dd, yyyy hh:mm a');
+    const formattedStartTime = format(new Date(startTime), 'EEE, dd MMMM yyyy - HH:mm');
+    const formattedEndTime = format(new Date(endTime), 'EEE, dd MMMM yyyy - HH:mm');
 
 
     return (
-        <div className="card pb-2 my-4">
+        <div className="card my-4">
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src={url} className="card-img" alt=""/>
                 </div>
-                <div className="col-md-4">
-                    <div className="card-body">
-                        <h5 className="card-title">{auction.product.name}</h5>
-                        <p className="card-text">{description}</p>
-                       
+                <div className="col-md-8">
+                    <div className="row">
+                        <div className="col-md-12 mb-3">
+                            <h5 className="card-title mb-0">{auction.product.name}</h5> 
+                            <p className="card-text">by {username}</p>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        <Link to="" className="btn btn-dark">View</Link>
-                    </div>             
-                </div>
-                <div className="col-md-4">
-                    <ul className="list-group list-group-flush">   
-                        <li className="list-group-item">Starts: {formattedStartTime}</li>
-                        <li className="list-group-item">Ends: {formattedEndTime}</li>
-                        <li className="list-group-item">Price: <strong>{startingPrice} €</strong></li>
-                        <li className="list-group-item">Bids: {bids.length > 0 ? bids.length : 0}</li>
-                    </ul>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h5 className="card-text"><strong>{startingPrice} €</strong></h5>
+                            <p className="card-text">{bids.length > 0 ? bids.length : 0} bids</p>
+                            <Link to="" className="btn btn-dark">View</Link> 
+                        </div>
+                        <div className="col-md-6">
+                            <p className="card-text">Starts: {formattedStartTime}</p>
+                            <p className="card-text">Ends: {formattedEndTime}</p>
+                        </div>        
+                    </div>
                 </div>
             </div>
         </div>  
--- auction-ui/src/routes/home/AuctionsList.component.jsx
+++ auction-ui/src/routes/home/AuctionsList.component.jsx
@@ -3,13 +3,14 @@ import { Container, Row, Col } from 'react-bootstrap';
 import AuctionCard from "./AuctionCard.component.jsx"
 
 const AuctionList = ({auctions}) => {
+    console.log(auctions)
 
     return (
         <Container>
             <div className="row justify-content-center">
                 <div className="col-10">
                     {auctions.map((auction) => (
-                        <AuctionCard auction={auction} />
+                        <AuctionCard auction={auction} key={auction.id} />
                     ))}
                 </div>
             </div>
